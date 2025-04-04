b3424eabd27164c3ec0ea35b429b4e88882f1e8c
joelruiz28 <joel.encinos69@unach.mx>
2025-04-04 03:06:33 -0600

    aw

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
 
 export default defineConfig({
-  base: '/',  // Ajusta esto si tu proyecto está en una subcarpeta
-  plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true
