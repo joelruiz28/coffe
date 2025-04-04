362614ec58c8913c8a55b7347c7440bcea78be0c
joelruiz28 <joel.encinos69@unach.mx>
2025-04-04 03:26:47 -0600

    se hizo el dist

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 
-
 export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'src/main.jsx',
+    },
+  },
 });
