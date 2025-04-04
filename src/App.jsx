fbfc42240710d0e316d72d4786ae1d4fd254a554
joelruiz28 <joel.encinos69@unach.mx>
2025-04-04 01:32:10 -0600

    se cambio app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Nosotros from "./pages/Nosotros"
 import Contacto from "./pages/Contacto";
